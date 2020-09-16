eec893a80911dc6ee8712da8e46c2ae4489311cb
jm-glowienke <jmglowienke@gmail.com>
2020-09-16 11:23:43 +0200

    save and read game log finished

--- tools.py
+++ tools.py
@@ -1,4 +1,3 @@
-import json
 import time
 
 def initial_state():
@@ -19,28 +18,34 @@ def initial_state():
 
 # create subdirectory
 def save_game_log(history):
-    with open('./logs/log_'+time.strftime("%Y%m%d-%H%M%S"), 'w') as f:
-        for item in history:
-            f.write('%s\n' % item)
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    with open('./logs/log_'+timestamp, 'w') as f:
+        f.write('log_'+timestamp + '\n')
+        f.write('src_row src_col dest_row dest_col\n')
+        for row in history:
+            for item in row:
+                f.write('%s       ' % item[0])
+                f.write('%s       ' % item[1])
+            f.write('\n')
+            # f.write('%s\n' % row)
     return
 
 def read_game_log(filename):
-    history = []
-
-    # open file and read the content in a list
-    with open('./logs/'+filename, 'r') as filehandle:
-        for line in filehandle:
-            # remove linebreak which is the last character of the string
-            current = line[1:-3]
-            for item in current:
-                print(item, end = '')
-            print()
-            #print(type(current))
-            # add item to the list
-            history.append(current)
-    return history
-
-a = [[1,2,3],[4,5,6]]
-# save_game_log(a)
-
-read_game_log('log_20200915-181301')
+    try:
+        history = []
+        # open file and read the content in a list
+        with open('./logs/'+filename, 'r') as filehandle:
+            next(filehandle)
+            next(filehandle)
+            for line in filehandle: #skip first two lines
+                # remove linebreak which is the last character of the string
+                current = line[:-1].replace(' ','')
+                #print(current)
+                save = []
+                for item in current:
+                    save.append(int(item))
+                print(save)
+                history.append([[save[0],save[1]],[save[2],save[3]]])
+        return history
+    except FileNotFoundError:
+        print("File or 'logs' folder not found!")
